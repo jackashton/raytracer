e3919f4cc236fe13d2b422592939bb9c7d247086
Jack Ashton <jackashton@Jacks-MacBook-Pro.local>
2022-05-15 17:28:03 +1200

    Takes filename as argument and added dependency for terminal progress bar to show the elapsed time, eta, percentage completed for the scanlines

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+use std::env;
 use raytracer::write::{write_image};
 
 
 fn main() {
-    write_image(256, 256, "test.png")
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Incorrect arguments, usage: {} <filename>", args[0]);
+        return;
+    }
+
+    let filename = &args[1];
+
+    write_image(256, 256, filename)
 }
--- src/write.rs
+++ src/write.rs
@@ -1,9 +1,21 @@
 use image::{RgbImage, ImageBuffer, Rgb};
+use indicatif::{ProgressBar, ProgressStyle};
 
 pub fn write_image(height: u32, width: u32, filename: &str) {
     let mut buffer: RgbImage = ImageBuffer::new(width, height);
 
+    let bar = ProgressBar::new(height as u64);
+    bar.set_style(ProgressStyle::default_bar().template(
+        "[{elapsed} elapsed] {wide_bar} {percent}% [{eta} remaining] writing to file"
+    ));
+
+    let mut prev: u32 = 0;
     for (x, y, pixel) in buffer.enumerate_pixels_mut() {
+        if y != prev {
+            prev = y;
+            bar.inc(1);
+        }
+
         let r = x as f64 / (width - 1) as f64;
         let g = y as f64 / (height - 1) as f64;
         let b = 0.25;
@@ -14,6 +26,8 @@ pub fn write_image(height: u32, width: u32, filename: &str) {
 
         *pixel = Rgb([ir, ig, ib]);
     }
+
+    bar.finish();
 
     match buffer.save(filename) {
         Err(e) => eprintln!("Error writing to file {}", e),
