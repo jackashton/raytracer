33ce7761f53e7ffac88c33f5a7663f267e70eb5c
Jack Ashton <jack.ashton.nz@gmail.com>
2022-05-17 22:46:38 +1200

    Restricted generic N in Vec3 to num_traits::Float type so that we can implement a length function as vector magniude requires the sqrt function. Added tests and more methods

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,32 +1,33 @@
 use std::ops::{Add, Sub, Mul, Div};
-use num_traits::{Num};
+use num_traits::{Float};
 
 #[derive(Debug, Copy, Clone)]
-pub struct Vec3<N: Num> {
+pub struct Vec3<N: Float> {
     pub x: N,
     pub y: N,
     pub z: N,
 }
 
-impl<N: Num> Vec3<N> where N: Copy {
+impl<N: Float> Vec3<N> {
     pub fn new(x: N, y: N, z: N) -> Vec3<N> {
         Vec3 { x, y, z }
     }
 
-    pub fn x(&self) -> N {
-        self.x
+    /// Magnitude of the vector
+    pub fn length(&self) -> N {
+        ((self.x * self.x) + (self.y * self.y) + (self.z * self.z)).sqrt()
     }
 
-    pub fn y(&self) -> N {
-        self.y
+    pub fn unit_vector(v: Vec3<N>) -> Vec3<N> {
+        v / v.length()
     }
 
-    pub fn z(&self) -> N {
-        self.z
+    pub fn dot(v: &Vec3<N>, u: &Vec3<N>) -> N {
+        (v.x * u.x) + (v.y * u.y) + (v.z * u.z)
     }
 }
 
-impl<N: Num> Add for Vec3<N> {
+impl<N: Float> Add for Vec3<N> {
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
@@ -34,7 +35,15 @@ impl<N: Num> Add for Vec3<N> {
     }
 }
 
-impl<N: Num> Sub for Vec3<N> {
+impl<N: Float> Add<N> for Vec3<N> {
+    type Output = Self;
+
+    fn add(self, rhs: N) -> Self::Output {
+        Self { x: self.x + rhs, y: self.y + rhs, z: self.z + rhs }
+    }
+}
+
+impl<N: Float> Sub for Vec3<N> {
     type Output = Self;
 
     fn sub(self, rhs: Self) -> Self::Output {
@@ -42,7 +51,47 @@ impl<N: Num> Sub for Vec3<N> {
     }
 }
 
-impl<N: Num> PartialEq for Vec3<N> {
+impl<N: Float> Sub<N> for Vec3<N> {
+    type Output = Self;
+
+    fn sub(self, rhs: N) -> Self::Output {
+        Self { x: self.x - rhs, y: self.y - rhs, z: self.z - rhs }
+    }
+}
+
+impl<N: Float> Mul<Vec3<N>> for Vec3<N> {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self { x: self.x * rhs.x, y: self.y * rhs.y, z: self.z * rhs.z }
+    }
+}
+
+impl<N: Float> Mul<N> for Vec3<N> {
+    type Output = Self;
+
+    fn mul(self, rhs: N) -> Self::Output {
+        Self { x: self.x * rhs, y: self.y * rhs, z: self.z * rhs }
+    }
+}
+
+impl<N: Float> Div<Vec3<N>> for Vec3<N> {
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        Self { x: self.x / rhs.x, y: self.y / rhs.y, z: self.z / rhs.z }
+    }
+}
+
+impl<N: Float> Div<N> for Vec3<N> {
+    type Output = Self;
+
+    fn div(self, rhs: N) -> Self::Output {
+        Self { x: self.x / rhs, y: self.y / rhs, z: self.z / rhs }
+    }
+}
+
+impl<N: Float> PartialEq for Vec3<N> {
     fn eq(&self, other: &Self) -> bool {
         self.x == other.x && self.y == other.y && self.z == other.z
     }
@@ -62,4 +111,44 @@ fn test_sub() {
     let vec2: Vec3<f64> = Vec3::new(0.1, 0.2, 0.4);
     let res: Vec3<f64> = Vec3::new(0.1, 0.2, 0.4);
     assert_eq!(vec1 - vec2, res);
+}
+
+#[test]
+fn test_mul() {
+    let vec1: Vec3<f64> = Vec3::new(1.0, 2.0, 3.0);
+    let vec2: Vec3<f64> = Vec3::new(2.0, 2.0, 2.0);
+    let res: Vec3<f64> = Vec3::new(2.0, 4.0, 6.0);
+    assert_eq!(vec1 * vec2, res);
+    assert_eq!(vec1 * 2.0, res);
+}
+
+#[test]
+fn test_div() {
+    let vec1: Vec3<f64> = Vec3::new(1.0, 2.0, 4.0);
+    let vec2: Vec3<f64> = Vec3::new(2.0, 2.0, 2.0);
+    let res: Vec3<f64> = Vec3::new(0.5, 1.0, 2.0);
+    assert_eq!(vec1 / vec2, res);
+    assert_eq!(vec1 / 2.0, res);
+}
+
+#[test]
+fn test_length() {
+    let vec1: Vec3<f64> = Vec3::new(0.0, 3.0, 4.0);
+    let res: f64 = 5.0;
+    assert_eq!(vec1.length(), res);
+}
+
+#[test]
+fn test_unit_vector() {
+    let vec1: Vec3<f64> = Vec3::new(0.0, 3.0, 4.0);
+    let res: Vec3<f64> = Vec3::new(0.0, 3.0 / 5.0, 4.0 / 5.0);
+    assert_eq!(Vec3::unit_vector(vec1), res);
+}
+
+#[test]
+fn test_dot_product() {
+    let vec1: Vec3<f64> = Vec3::new(1.0, 3.0, 4.0);
+    let vec2: Vec3<f64> = Vec3::new(2.0, 3.0, 5.0);
+    let res: f64 = 31.0;
+    assert_eq!(Vec3::dot(&vec1, &vec2), res);
 }
