815702247918b2fb2a0ad957a55fdb8293ee9df9
Jack Ashton <jack.ashton.nz@gmail.com>
2023-03-27 23:09:22 +1300

    Total internal reflection

--- src/main.rs
+++ src/main.rs
@@ -70,9 +70,9 @@ fn main() {
     let mut world: HittableList<dyn Hittable> = HittableList::new();
 
     let material_ground = Lambertian::new(Vec3::new(0.8, 0.8, 0.0));
-    let material_center = Dielectric::new(1.5);
+    let material_center = Lambertian::new(Vec3::new(0.1, 0.2, 0.5));
     let material_left = Dielectric::new(1.5);
-    let material_right = Metal::new(Vec3::new(0.8, 0.6, 0.2), 1.0);
+    let material_right = Metal::new(Vec3::new(0.8, 0.6, 0.2), 0.0);
 
     world.push(Box::new(Sphere::new(
         Point3::new(0.0, -100.5, -1.0),
