7305171f78cd5ac1a6a6855ade311ecae4d0c3b7
Jack Ashton <jack.ashton.nz@gmail.com>
2023-04-01 13:40:15 +1300

    Positioning and orienting the camera. Camera Refactor

--- src/objects/camera.rs
+++ src/objects/camera.rs
@@ -9,18 +9,25 @@ pub struct Camera {
 }
 
 impl Camera {
-    pub fn new(vfov: f64, aspect_ratio: f64) -> Camera {
+    pub fn new(
+        origin: Point3<f64>,
+        target: Point3<f64>,
+        vup: Vec3<f64>,
+        vfov: f64,
+        aspect_ratio: f64,
+    ) -> Camera {
         let theta = vfov.to_radians();
         let h = (theta / 2.0).tan();
         let viewport_height = 2.0 * h;
         let viewport_width = aspect_ratio * viewport_height;
-        let focal_length = 1.0;
 
-        let origin: Point3<f64> = Point3::zero();
-        let horizontal: Vec3<f64> = Vec3::new(viewport_width, 0.0, 0.0);
-        let vertical: Vec3<f64> = Vec3::new(0.0, viewport_height, 0.0);
-        let lower_left_corner =
-            origin - (horizontal / 2.0) - (vertical / 2.0) - Vec3::new(0.0, 0.0, focal_length);
+        let w = (origin - target).normalize();
+        let u = vup.cross(&w).normalize();
+        let v = w.cross(&u);
+
+        let horizontal = u * viewport_width;
+        let vertical = v * viewport_height;
+        let lower_left_corner = origin - (horizontal / 2.0) - (vertical / 2.0) - w;
 
         Camera {
             origin,
